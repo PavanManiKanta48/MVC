3014bd1d0c71eaa5da6946628615f5ad25f08efe
PavanManiKanta48 <pavanmanikanta484@gmail.com>
2022-12-23 10:34:52 +0530

    CodeFirst_GetOperation

---
+++ BulkBook/BulkBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkBookWeb.Data;
+using BulkBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategorylist = _dbContext.categories;
+            return View(objCategorylist);
+        }
+    }
+}
---
+++ BulkBook/BulkBookWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+#pragma warning disable CS8618 // Non-nullable property 'Name' must contain a non-null value when exiting constructor. Consider declaring the property as nullable.
+        public string Name { get; set; }
+#pragma warning restore CS8618 // Non-nullable property 'Name' must contain a non-null value when exiting constructor. Consider declaring the property as nullable.
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
